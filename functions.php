73da2ea7722a3a0ab18bd66ab070a7f20b394740
Ashutosh8454 <ashutosh703325@gmail.com>
2023-04-21 09:57:05 +0530

    adding css folder and scss link and compile

--- functions.php
+++ functions.php
@@ -139,6 +139,9 @@ add_action( 'widgets_init', 'smaago_widgets_init' );
  */
 function smaago_scripts() {
 	wp_enqueue_style( 'smaago-style', get_stylesheet_uri(), array(), _S_VERSION );
+
+	wp_enqueue_style( 'smaago-main', get_template_directory_uri(). '/css/main.css' );
+
 	wp_style_add_data( 'smaago-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'smaago-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
